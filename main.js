ca2fee8f946cdc511b4990cc77d33c129221a020
rosnowski <143711247+rosnowski@users.noreply.github.com>
2025-06-06 09:21:22 -0300

    ok

--- main.js
+++ main.js
@@ -13,7 +13,40 @@ botoes[i].onclick = function () {
     textos[j].classList.remove("ativo");
 }
 }
+  
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2026-01-01T00:00:00");
+const tempoObjetivo2 = new Date("2025-31-12T00:00:00");
+const tempoObjetivo3 = new Date("2032-07-12T00:00:00");
+const tempoObjetivo4 = new Date("2027-20-08T00:00:00");
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
     
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
     
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+}
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);
+}
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
     
-    
+comecaCronometro();    
